4b793653adc7bace6472ce1839eb41a9a91f4e6a
Anthony <60526029+anthonylopes54@users.noreply.github.com>
2020-04-11 23:21:55 -0700

    Updates

---
+++ precache-manifest.3b064613692c9beb9ac57473d708bf1c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7db873c9de54fc673a88e8c4b81ed0a",
+    "url": "/Ravenous-React-App/index.html"
+  },
+  {
+    "revision": "41b22d1ff2ba2efe4f10",
+    "url": "/Ravenous-React-App/static/css/main.8c80162d.chunk.css"
+  },
+  {
+    "revision": "ea06a956457c6f6975cf",
+    "url": "/Ravenous-React-App/static/js/2.e354febf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Ravenous-React-App/static/js/2.e354febf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41b22d1ff2ba2efe4f10",
+    "url": "/Ravenous-React-App/static/js/main.524439f7.chunk.js"
+  },
+  {
+    "revision": "8deb4ada6b6f2fe56f68",
+    "url": "/Ravenous-React-App/static/js/runtime-main.ec4dd375.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/Ravenous-React-App/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/Ravenous-React-App/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
